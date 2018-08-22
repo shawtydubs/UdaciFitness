9498fb2c78ea9285a74c861986ad8f49eeec18a6
Claire Walters <claire.walters@opower.com>
2018-08-21 20:47:34 -0400

    Add screen for alreadyLogged

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import {getMetricMetaInfo, timeToString} from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import TextButton from './TextButton';
 
 function SubmitBtn ({onPress}) {
     return (
@@ -77,8 +79,35 @@ export default class AddEntry extends Component {
         // Clear local notification
     }
 
+    reset = () => {
+        const key = timeToString();
+
+        // Update Redux
+
+        // Route to Home
+
+        // Update "DB"
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo();
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                        name={'ios-happy-outline'}
+                        size={100}
+                    />
+                    <Text>
+                        You already logged your information for today.
+                    </Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
 
         return (
             <View>
