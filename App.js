117c0cd5fe14431db2420abd58b14c403538f453
Claire Walters <claire.walters@opower.com>
2018-08-26 18:44:19 -0400

    Fix stacked navigation

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { View, Platform, StatusBar } from 'react-native'
-import AddEntry from './components/AddEntry'
+import { View, StatusBar, Platform} from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import History from './components/History'
-import { createBottomTabNavigator, createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation'
 import { purple, white } from './utils/colors'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import TabsIos from './components/TabsIos'
+import TabsAndroid from './components/TabsAndroid'
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
     return (
@@ -19,9 +18,13 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
     )
 }
 
+const nav = Platform.OS === 'ios'
+    ? TabsIos
+    : TabsAndroid
+
 const MainNavigator = createStackNavigator({
     Home: {
-        screen: Tabs,
+        screen: nav,
         navigationOptions: {
             header: null
         }
@@ -36,47 +39,6 @@ const MainNavigator = createStackNavigator({
         }
     }
 })
-
-const RouteConfigs = {
-    History: {
-        screen: History,
-        navigationOptions: {
-            tabBarLabel: 'History',
-            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-      },
-    },
-    AddEntry: {
-        screen: AddEntry,
-        navigationOptions: {
-            tabBarLabel: 'Add Entry',
-            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-        },
-    },
-}
-
-const TabNavigatorConfig = {
-    navigationOptions: {
-        header: null
-    },
-    tabBarOptions: {
-        activeTintColor: Platform.OS === "ios" ? purple : white,
-        style: {
-            height: 56,
-            backgroundColor: Platform.OS === "ios" ? white : purple,
-            shadowColor: "rgba(0, 0, 0, 0.24)",
-            shadowOffset: {
-                width: 0,
-                height: 3
-            },
-            shadowRadius: 6,
-            shadowOpacity: 1
-        }
-    }
-};
-
-const Tabs = Platform.OS === "ios"
-    ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
-    : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
 
 export default class App extends React.Component {
     render() {
