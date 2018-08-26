9998a235a831e450f041ae305b5053e249b4fd73
Claire Walters <claire.walters@opower.com>
2018-08-26 17:25:38 -0400

    Add navigation

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import {View} from 'react-native';
-import AddEntry from './components/AddEntry';
-import { createStore } from 'redux';
-import {Provider} from 'react-redux';
-import reducer from './reducers';
-import History from './components/History';
+import React from 'react'
+import { View, Platform } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
+import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+const RouteConfigs = {
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+      },
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+        },
+    },
+}
+
+const TabNavigatorConfig = {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === "ios" ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? white : purple,
+            shadowColor: "rgba(0, 0, 0, 0.24)",
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+};
+
+const Tabs = Platform.OS === "ios"
+    ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+    : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <Provider store={createStore(reducer)}>
-                <View style={{flex: 1}}>
-                    <History />
-                </View>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <Tabs />
+        </View>
+      </Provider>
+    )
+  }
 }
