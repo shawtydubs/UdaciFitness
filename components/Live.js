8751a778d934b2216c24a5ca79f89e326d1b07e1
Claire Walters <claire.walters@opower.com>
2018-08-27 16:34:45 -0400

    Add UI for undetermined Live status

--- components/Live.js
+++ components/Live.js
@@ -1,35 +1,80 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
- export default class Live extends Component {
-  state = {
-    coords: null,
-    status: null,
-    direction: ''
-  }
-  render() {
-    const { status, coords, direction } = this.state
-     if (status === null) {
-      return <ActivityIndicator style={{marginTop: 30}}/>
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
+
+export default class Live extends Component {
+    state = {
+        coords: null,
+        status: 'undetermined',
+        direction: ''
     }
-     if (status === 'denied') {
-      return (
-        <View>
-          <Text>Denied</Text>
-        </View>
-      )
+
+    askPermission = () => {
+
     }
-     if (status === 'undetermined') {
-      return (
-        <View>
-          <Text>undetermined</Text>
-        </View>
-      )
+
+    render() {
+        const { status, coords, direction } = this.state
+
+        if (status === null) {
+            return <ActivityIndicator style={{marginTop: 30}}/>
+        }
+
+        if (status === 'denied') {
+            return (
+                <View>
+                    <Text>Denied</Text>
+                </View>
+            )
+        }
+
+        if (status === 'undetermined') {
+            return (
+                <View style={styles.center}>
+                    <Foundation name='alert' size={50} />
+                    <Text>
+                        You need to enable location services for this app.
+                    </Text>
+                    <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+                        <Text style={styles.buttonText}>
+                            Enable
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+
+        return (
+            <View style={styles.container}>
+                <Text>Live</Text>
+                <Text>{JSON.stringify(this.state)}</Text>
+            </View>
+        )
     }
-     return (
-      <View>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
-      </View>
-    )
-  }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    button: {
+        padding: 10,
+        backgroundColor: purple,
+        alignSelf: 'center',
+        borderRadius: 5,
+        margin: 20,
+    },
+    buttonText :{
+        color: white,
+        fontSize: 20,
+    }
+});
